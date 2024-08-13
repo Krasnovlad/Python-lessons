da45d15cb5fa97354a551e64291fef247d7f953f
Krasnovlad <videology@yandex.ru>
2024-08-13 21:47:06 +0300

    Add files via upload

---
+++ lesson 002.py
@@ -0,0 +1,5 @@
+count_lessons = 12
+total_time = 1.5
+name_course = 'Puthon'
+lesson_time = (total_time / count_lessons)
+print('Курс:', name_course, ' Всего задач:',count_lessons, ' Затрачено часов:', total_time, ' Среднее время выполнения',  lesson_time)
