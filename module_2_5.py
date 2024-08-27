5baf1704c7e80f11f8cd5d87cb6e89b00bee1329
Krasnovlad <videology@yandex.ru>
2024-08-27 18:01:36 +0300

    Update module_2_5.py

--- module_2_5.py
+++ module_2_5.py
@@ -2,11 +2,9 @@ def get_matrix(n, m, value):
     matrix = []
     for i in range(n):
         list_ = list()
-        matrix.append(list_)
-        i += 1
+        matrix.append(list_
         for j in range(m):
             list_.append(value)
-            j += 1
     return matrix
 
 result1 = get_matrix(2, 2,10)
